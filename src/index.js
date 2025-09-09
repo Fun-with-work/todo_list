e788bf654fcab2d42fcd7f3f4f5cf68471ec7071
aaman <aneela.aman.is@gmail.com>
2025-09-09 12:50:57 -0400

    create files for modular JS components & their css

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 import "./styles.css";
+import "./header.js";
+import "./side-menu.js";
+import "./main-content.js";
+import "./footer.js";
+
 import {list} from "./greeting.js";
 
 console.log(list);
@@ -7,4 +12,5 @@ import theImage from "./images/restaurant-img.jpg";
 
 const img = document.createElement("img");
 img.src = theImage;
+img.id = "restaurant-image";
 document.body.appendChild(img);
