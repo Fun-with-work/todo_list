751b4ed5b9058e194999f28619ce76a743e21268
aaman <aneela.aman.is@gmail.com>
2025-11-25 13:00:26 -0500

    Add create footer function

--- src/footer.js
+++ src/footer.js
@@ -1,5 +1,9 @@
 import "./footer.css";
 
+const content = document.getElementById("content");
+
+export function createFooter() {
+
 const footer = document.createElement("footer");
 footer.id = "footer";
 footer.innerHTML = `
@@ -9,4 +13,5 @@ footer.innerHTML = `
         <p id="footer-message">Images: <a href="https://pixabay.com/users/mediamodifier-1567646/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=2108029">Mediamodifier</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=2108029">Pixabay, </a> <a href="https://pixabay.com/users/tumisu-148124/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=1919292">Tumisu</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=1919292">Pixabay</a></p>
     </div>
 `;
-document.body.appendChild(footer);
+content.appendChild(footer);
+}
