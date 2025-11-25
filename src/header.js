e9edd4f7cab6de28fdac12d5b7def89a1c2c9834
aaman <aneela.aman.is@gmail.com>
2025-11-25 12:58:37 -0500

    Add createHeader functions

--- src/header.js
+++ src/header.js
@@ -1,12 +1,17 @@
 import "./header.css";
 
+const content = document.getElementById("content");
+
+export function createHeader() {
+
 const header = document.createElement("header");
 header.id = "header";
 header.className = "header";
-document.body.appendChild(header);
+content.appendChild(header);
 
 const taskBar = document.createElement("div");
 taskBar.id = "task-bar";
 taskBar.className = "task-bar";
 taskBar.textContent = "Current Project: All Tasks";
 header.appendChild(taskBar);
+}
