02ec89558cc68e442e8e2c09a6ee4dcca97aa82e
aaman <aneela.aman.is@gmail.com>
2025-11-25 12:50:04 -0500

    add varaiables for main layout div and append to content

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 import "./styles.css";
-import  "./header.js";
-import "./side-menu.js";
-import "./main-content.js";
-import "./footer.js";
+
+import { createHeader } from "./header.js";
+import { createSideMenu, addTaskButton } from "./side-menu.js";
+import { createMainContent } from "./main-content.js";
+import { createFooter } from "./footer.js";
 
 import {list} from "./greeting.js";
 
 console.log(list);
 
+const content = document.getElementById("content");
+const headerElement = createHeader();
+const sideMenuElement = createSideMenu();
+const mainContentElement = createMainContent();
+const footerElement = createFooter();
 
+const mainLayout = document.createElement("div");
+mainLayout.id = "main-layout";
+mainLayout.className = "main-layout";
+
+content.appendChild(mainLayout);
+
+mainLayout.appendChild(sideMenuElement);
+mainLayout.appendChild(mainContentElement);
+
+import "./add-todo.js";
