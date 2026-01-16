27f5192e6a22b25876a1f0b1cf4509bb9ef89e73
aaman <aneela.aman.is@gmail.com>
2026-01-16 13:22:30 -0500

    new file for search button functionality

---
+++ src/search-todo.js
@@ -0,0 +1,5 @@
+import  "./search-todo.css";
+import { searchButton } from "./side-menu";
+import { createMainContent } from "./main-content.js";  
+import { displaySearchResults } from "./task-list.js";
+import { createTodoForm } from "./create-todo-form.js";
